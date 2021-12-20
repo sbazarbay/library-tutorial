cc27dc49d509a8292ed5712b9fa6ae1716d66adc
Cheena <c.bazarbai@gmail.com>
2021-12-20 18:47:42 +0600

    Added Author resource

---
+++ app/Models/Author.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Author extends Model
+{
+    protected $guarded = [];
+
+    protected $dates = ['dob'];
+}
---
+++ tests/Feature/AuthorManagementTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Author;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthorManagementTest extends TestCase
+{
+    use RefreshDatabase;
+    /** @test */
+    public function an_author_can_be_created()
+    {
+        $this->withoutExceptionHandling();
+        $this->post('/author', [
+            'name' => 'Author Name',
+            'dob' => '05/14/1988',
+        ]);
+
+        $authors = Author::all();
+
+        $this->assertCount(1, $authors);
+        $this->assertInstanceOf(Carbon::class, $authors->first()->dob);
+        $this->assertEquals('1988/14/05', $authors->first()->dob->format('Y/d/m'));
+    }
+}
